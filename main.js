36db54c43233183567a850037f51df708544910b
Karla <terratinomexico@MacBook-Pro-de-Karla.local>
2022-09-07 11:00:00 -0500

    Se sgrega el proyesto

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav_menu").classList.toggle("show");
+  });
+
+  ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.card-dashboard', { delay: 500 });
+ScrollReveal().reveal('.banner-section-one"', { delay: 500 });
+ScrollReveal().reveal('.card-banner-two', { delay: 500 });
+  
